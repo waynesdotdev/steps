add7639e2d419d35e04863ab4cdb81d983b6f5f0
waynesdotdev <fox.wayne1@gmail.com>
2023-06-06 13:17:59 +0100

    state is working and counting correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,14 +7,21 @@ const messages = [
 ]
 
 export default function App() {
-  const step = 1
+  const [step, setStep] = useState(1)
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1)
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1)
+  }
 
   return (
     <div className='steps'>
       <div className='numbers'>
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
 
       <p className='message'>
@@ -20,10 +29,16 @@ export default function App() {
       </p>
 
       <div className='buttons'>
-        <button style={{ backgroundColor: '#7950f2', color: 'white' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: 'white' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: 'white' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: 'white' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
