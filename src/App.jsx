5a6c3556debea23a49ff8c00a761f99b87dee91b
waynesdotdev <fox.wayne1@gmail.com>
2023-06-07 06:34:10 +0100

    update to way it counts

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true)
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1)
+    if (step > 1) setStep(s => s - 1)
   }
   function handleNext() {
     if (step < 3) setStep(step + 1)
